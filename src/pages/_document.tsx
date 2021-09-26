94d41a5ff5f696eb83152a4276cc37cf800b41dd
takagi <iam.takagi.dev@gmail.com>
2021-09-27 05:40:13 +0900

    Remove viewport

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,10 +7,6 @@ class MyDocument extends Document {
       <Html lang="ja">
         <Head>
           <meta charSet="utf-8" />
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
           <title>biomex</title>
         </Head>
         <body>
