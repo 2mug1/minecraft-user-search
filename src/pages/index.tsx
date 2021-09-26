78c0274234d67f9b110462079519effdb0536dd9
takagi <iam.takagi.dev@gmail.com>
2021-09-27 05:38:29 +0900

    Fix マイクラのユーザー名を検索するやつです。 to マイクラの名前履歴を検索するやつです。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 const IndexPage: React.FC<{}> = () => (
     <div className="mt-5">
-        <p> マイクラのユーザー名を検索するやつです。<br/>
+        <p>マイクラの名前履歴を検索するやつです。<br/>
         ソースコードは <a href="https://github.com/iamtakagi/biomex" className="border-b hover:border-purple-800 text-purple-800 border-transparent">こちら</a> に置いています。</p>
     </div>
 )
