8d7576d5294a9e49df2ce78bd8edf7518e9e141f
takagi <iam.takagi.dev@gmail.com>
2022-02-20 20:24:33 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 const IndexPage: React.FC<{}> = () => (
     <div className="mt-5">
-        <p>マイクラのユーザーを検索できるやつです。<br/>
+        <p>マイクラのユーザーを検索できるサービスです。<br/>
         ソースコードは <a href="https://github.com/iamtakagi/minecraft-user-search" className="border-b hover:border-purple-800 text-purple-800 border-transparent">こちら</a> に置いています。</p>
     </div>
 )
